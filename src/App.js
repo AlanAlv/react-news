52e9986076b60b532cb602534e8260c5c687a86b
AlanAlv <alan.alv96@gmail.com>
2020-06-05 03:17:19 -0500

    API call

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Header from './components/Header';
 import Form from './components/Form';
 
@@ -7,6 +7,25 @@ import Form from './components/Form';
 function App() {
 
   const [ category, saveCategory ] = useState('');
+  const [ news, saveNews ] = useState({});
+
+  useEffect(() => {
+    const callAPI = async () => {
+
+      const url = `http://newsapi.org/v2/top-headlines?country=mx&category=${category}&apiKey=62c87eb83a7741359cb883c5cfa0e708`;
+    
+      const answer = await fetch(url);
+      const news = await answer.json()
+
+      console.log(news);
+      saveNews(news);
+    };
+
+    callAPI();
+
+
+    
+  }, [category])
 
   return (
     <Fragment>
